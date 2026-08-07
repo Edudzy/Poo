134ac2138f50fc03f45ca4a7a347c90b34bab544
Edudzy <162237328+Edudzy@users.noreply.github.com>
2026-08-07 20:39:20 -0300

    Add files via upload

---
+++ poo050826/poo070826/src/main/java/com/mycompany/poo070826/Poo070826.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo070826;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class Poo070826 {
+
+    public static void main(String[] args) {
+        Conta c1 = new Conta(222, 898, 8000.00);
+        System.out.println("Agencia: "+ c1.getAgencia());
+        System.out.println("Numero: "+ c1.getNumero());
+        System.out.println("Saldo da conta: "+ c1.getSaldo());
+        
+        
+    }
+}
