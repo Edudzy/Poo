47c1b2e3952f0f4e3c7b4b923111ef552e856559
Edudzy <162237328+Edudzy@users.noreply.github.com>
2026-08-21 20:35:44 -0300

    Add files via upload

---
+++ poo210826/src/main/java/com/mycompany/poo210826/ContaCorrente.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo210826;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class ContaCorrente extends Conta{
+    private double limite;
+    
+    public ContaCorrente(){
+        
+    }
+    
+    public ContaCorrente(int agencia, int numero, double saldo, double limite){
+        //super.setAgencia(agencia);
+        //super.setNumero(numero);
+        //super.setSaldo(saldo);
+        super(agencia,numero,saldo);
+        this.limite = limite;
+    }
+    public void atualizarLimite(double limite){
+        if(limite>=0){
+            this.limite = limite;
+        }
+        
+        //Consertar o ML/Atualizar limite, ele não está completo
+    }
+}
---
+++ poo210826/src/main/java/com/mycompany/poo210826/Poo210826.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo210826;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class Poo210826 {
+
+    public static void main(String[] args) {
+    Conta c1 = new Conta(1,1,1000.00);
+    Conta c2 = new Conta(1,2,890.00);
+        
+    c1.creditar(800);
+    c2.debitar(900);
+    c1.transferir(1500, c2);
+        
+    System.out.println("O saldo da Conta 1: " + c1.getSaldo());
+    System.out.println("O saldo da Conta 2: " + c2.getSaldo());
+        
+    Conta c1 = new ContaCorrente();
+    ContaCorrente c1 = new Conta();
+    }
+}
